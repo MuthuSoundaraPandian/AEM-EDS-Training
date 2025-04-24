5577b4fd38dc4cbd2cf599026dae07a18c057845
MuthuSoundaraPandian <soundhar.sms1992@gmail.com>
2025-04-24 11:35:12 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
   root: true,
-  extends: 'airbnb-base',
   env: {
     browser: true,
   },
+  extends: 'airbnb-base',
   parser: '@babel/eslint-parser',
   parserOptions: {
-    allowImportExportEverywhere: true,
-    sourceType: 'module',
     requireConfigFile: false,
+    sourceType: 'module',
+    allowImportExportEverywhere: true,
   },
   rules: {
-    'import/extensions': ['error', { js: 'always' }], // require js file extensions in imports
-    'linebreak-style': ['error', 'unix'], // enforce unix linebreaks
-    'no-param-reassign': [2, { props: false }], // allow modifying properties of param
+    'import/extensions': ['error', 'always'], // require js file extensions in imports
+    'linebreak-style': ['error', 'unix'],     // enforce unix linebreaks
+    'no-param-reassign': ['error', { props: false }], // allow modifying properties of params
   },
 };
