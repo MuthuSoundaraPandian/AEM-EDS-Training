57c89bfc1871bfa6a5065a9449b2a5621c36a19f
arunprasath1993 <arunprasath.sysarc@gmail.com>
2025-04-27 10:01:19 +0530

    quotes issue fixed

--- blocks/essential-links/essential-links.js
+++ blocks/essential-links/essential-links.js
@@ -1,10 +1,10 @@
 export default function decorate(block) {
   const title = block.children[0];
-  title.classList.add("d-flex-wrapper");
+  title.classList.add('d-flex-wrapper');
 
   const imgbox = block.children[0].children[0];
-  imgbox.classList.add("img-div");
+  imgbox.classList.add('img-div');
 
   const textcontent = block.children[0].children[1];
-  textcontent.classList.add("grey-bg");
+  textcontent.classList.add('grey-bg');
 }
