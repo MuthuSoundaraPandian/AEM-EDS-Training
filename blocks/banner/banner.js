8f4ad37f634869d784ed3ecabe7a9640374a9efe
ajithak0519 <ajithkishore519@gmail.com>
2025-04-27 09:36:46 +0530

    banner file done

--- blocks/banner/banner.js
+++ blocks/banner/banner.js
@@ -1,27 +1,14 @@
-// export default function decorate(block) {
-//     const title = block.children[0].children[0].children[0];
-//     console.log(title);
-//     title.classList.add('title');
-//     const pretitle = block.children[1].children[0].children[0];
-//     console.log(pretitle);
-//     pretitle.classList.add('pretitle');
-//     const img = block.children[2].children[0];
-//     console.log(img);
-//     img.classList.add('img');
-// }
-
-
 export default function decorate(block) {
-    const title = block.children[0].children[0].children[0];
-    if (title) {
-        title.classList.add('title');
-    }    
-    const description = block.children[0].children[1].children[0];
-    if (description) {
-        description.classList.add('description');
-    }
-    const img = block.children[1].children[0];
-    if (img) {
-        img.classList.add('img');
-    }
+  const title = block.children[0].children[0].children[0];
+  if (title) {
+    title.classList.add("title");
+  }
+  const description = block.children[0].children[1].children[0];
+  if (description) {
+    description.classList.add("description");
+  }
+  const img = block.children[1].children[0];
+  if (img) {
+    img.classList.add("img");
+  }
 }
