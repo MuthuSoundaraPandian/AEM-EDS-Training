c236ece6b5a86edf0e618c85258ec9b654fffdfe
arunprasath1993 <arunprasath.sysarc@gmail.com>
2025-05-02 07:49:28 +0530

    Quotes issue fixed

--- blocks/essential-links-v2/essential-links-v2.js
+++ blocks/essential-links-v2/essential-links-v2.js
@@ -1,26 +1,27 @@
 export default function decorate(block) {
   const essentialHeading = block.children[0];
-  essentialHeading ? essentialHeading.classList.add("essential-heading") : null;
+  essentialHeading ? essentialHeading.classList.add('essential-heading') : null;
   const essentialImgContentContainer = block.children[1];
   if (essentialImgContentContainer) {
     essentialImgContentContainer.classList.add(
-      "essential_img_content_container"
+      'essential_img_content_container'
     );
   }
   const essentialImgSection = document.querySelectorAll(
-    ".essential_img_content_container > div:first-child"
+    '.essential_img_content_container > div:first-child'
   );
   if (essentialImgSection.length >= 0) {
     essentialImgSection.forEach((div) => {
-      div.classList.add("essential-img");
+      div.classList.add('essential-img');
     });
   }
   const essentialContentSection = document.querySelectorAll(
-    ".essential_img_content_container > div:last-child"
+    '.essential_img_content_container > div:last-child'
   );
   if (essentialContentSection.length >= 0) {
     essentialContentSection.forEach((div) => {
-      div.classList.add("essential-content");
+      div.classList.add('essential-content');
     });
   }
 }
+
