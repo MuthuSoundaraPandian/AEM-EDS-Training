d97a87419cb0e6aa6ade22d988df9c8b06dfcfe9
Divya Ganesan <divya.d.ganesan@accenture.com>
2025-05-12 19:35:41 +0530

    Teaser left and right component changes

--- blocks/teaser-left-image/teaser-left-image.js
+++ blocks/teaser-left-image/teaser-left-image.js
@@ -1,16 +1,30 @@
 export default function decorate(block) {
     const img = block.children[0];
-    img.classList.add('teaser-left-image__overlapimage');
+    if(img){
+        img.classList.add('teaser-left-image__overlapimage');
+    }
     const desc = block.children[1];
-    desc.classList.add('teaser-left-image__content');
+    if(desc){
+        desc.classList.add('teaser-left-image__content');
+    }
     const overlap = block.children[0].children[0];
-    overlap.classList.add('teaser-left-image__overlap-image');
+    if(overlap){
+        overlap.classList.add('teaser-left-image__overlap-image');
+    }
     const btn = block.children[1].children[2].children[0];
-    btn.classList.add('teaser-left-image__button');
+    if(btn){
+        btn.classList.add('teaser-left-image__button');
+    }
     const heading = block.children[1].children[1].children[0];
-    heading.classList.add('teaser-left-image__heading');
+    if(heading){
+        heading.classList.add('teaser-left-image__heading');
+    }
     const para = block.children[1].children[0].children[0];
-    para.classList.add('teaser-left-image__sub-heading');
+    if(para){
+        para.classList.add('teaser-left-image__sub-heading');
+    }
     const button = block.children[1].children[2].children[0].children[0];
-    button.classList.add('teaser-left-image__learnmore-btn'); 
+    if(button){
+        button.classList.add('teaser-left-image__learnmore-btn'); 
+    }
 }
--- blocks/teaser-right-image/teaser-right-image.js
+++ blocks/teaser-right-image/teaser-right-image.js
@@ -1,17 +1,30 @@
 export default function decorate(block) {
     const img = block.children[0];
-    img.classList.add('teaser-right-image__overlapimage');
+    if(img){
+        img.classList.add('teaser-right-image__overlapimage');
+    }
     const desc = block.children[1];
-    desc.classList.add('teaser-right-image__content');
+    if(desc){
+        desc.classList.add('teaser-right-image__content');
+    }
     const overlap = block.children[0].children[0];
-    overlap.classList.add('teaser-right-image__overlap-image');
+    if(overlap){
+        overlap.classList.add('teaser-right-image__overlap-image');
+    }
     const btn = block.children[1].children[2].children[0];
-    btn.classList.add('teaser-right-image__button');
+    if(btn){
+        btn.classList.add('teaser-right-image__button');
+    }
     const heading = block.children[1].children[1].children[0];
-    heading.classList.add('teaser-right-image__heading');
+    if(heading){
+        heading.classList.add('teaser-right-image__heading');
+    }
     const para = block.children[1].children[0].children[0];
-    para.classList.add('teaser-right-image__sub-heading');
+    if(para){
+        para.classList.add('teaser-right-image__sub-heading');
+    } 
     const button = block.children[1].children[2].children[0].children[0];
-    button.classList.add('teaser-right-image__learnmore-btn');
-    
+    if(button){
+        button.classList.add('teaser-right-image__learnmore-btn');
+    }
 }
